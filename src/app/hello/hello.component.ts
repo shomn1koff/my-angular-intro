746c02b4bbc094870ccfd68627fa046c49a51613
shomn1koff <lexxminer@gmail.com>
2025-05-21 01:05:39 +0300

    одностороннее, двустороннее связывание, обработчик событий ввода текста

---
+++ src/app/hello/hello.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import {FormsModule} from '@angular/forms';
+
+@Component({
+  selector: 'app-hello',
+  templateUrl: './hello.component.html',
+  imports: [
+    FormsModule
+  ],
+  styleUrls: ['./hello.component.scss']
+})
+export class HelloComponent {
+  username: string = '';
+
+  greet(): string {
+    if (this.username.trim().length === 0) {
+      return 'Пожалуйста, введите ваше имя';
+    }
+    return `Привет, ${this.username}! Добро пожаловать в Angular и TypeScript!`
+  }
+
+  onInputChange(event: Event) {
+    const input = event.target as HTMLInputElement;
+    this.username = input.value;
+  }
+}
