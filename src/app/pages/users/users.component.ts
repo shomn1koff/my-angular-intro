dc3255b71ad7283ba94b9ad08850b25e526c3a70
shomn1koff <lexxminer@gmail.com>
2025-05-21 18:11:49 +0300

    пагинация и получение пользаков с апи

--- src/app/pages/users/users.component.ts
+++ src/app/pages/users/users.component.ts
@@ -1,11 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TuiPagination } from '@taiga-ui/kit';
+import { UserService } from '../../services/user.service';
+import { User } from '../../interfaces/user.interface';
 
 @Component({
   selector: 'app-users',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, TuiPagination],
   templateUrl: './users.component.html',
-  styleUrl: './users.component.scss'
+  styleUrls: ['./users.component.scss']
 })
-export class UsersComponent {
+export class UsersComponent implements OnInit {
+  users: User[] = [];
+  currentPage = 0; // Изменили с 1 на 0
+  totalPages = 1;
+  itemsPerPage = 2;
 
+  constructor(private userService: UserService) {}
+
+  ngOnInit() {
+    this.loadUsers();
+  }
+
+  loadUsers() {
+    // Добавляем +1 к currentPage при вызове getUsers, так как API ожидает страницы с 1
+    this.userService.getUsers(this.currentPage + 1, this.itemsPerPage)
+      .subscribe(users => {
+        this.users = users;
+      });
+
+    this.userService.getTotalUsers()
+      .subscribe(users => {
+        this.totalPages = Math.ceil(users.length / this.itemsPerPage);
+      });
+  }
+
+  onPageChange(page: number) {
+    this.currentPage = page;
+    this.loadUsers();
+  }
 }
